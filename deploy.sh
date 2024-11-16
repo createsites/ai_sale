1a66757bb8fb7fb78bae4e91ea2af030f892b217
createsites <biryuk88@gmail.com>
2024-11-16 13:32:09 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Путь к директории на сервере, куда нужно развернуть приложение
-#APP_DIR=/var/www/myapp
+APP_DIR=www/prostogpt.alexarg.com
 
 # Обновляем код
-#cd $APP_DIR
+cd $APP_DIR
 git pull origin master
 
 # применяем миграции
