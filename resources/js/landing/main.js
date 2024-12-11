5a6e5a1df46c1a992e16d3376887e9a713d7ea25
Alexandr Novoselov <a.novoselov@informer.com>
2024-12-11 18:08:52 -0300

    Smoke doesn't using on mobile devices.

--- resources/js/landing/main.js
+++ resources/js/landing/main.js
@@ -1,14 +1,3 @@
-import { testWebP } from "./webp_detector.js";
-
-// Определяет поддержку webp изображений и добавляет класс для body
-testWebP((support) => {
-    if (support === true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
-});
-
 // Плавный скролл при клике на пункты меню
 document.querySelectorAll('.nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
@@ -28,99 +17,14 @@ document.querySelectorAll('.faq-question').forEach((question) => {
     });
 });
 
-// Эффект стелющегося дыма с градиентом
-const canvas = document.createElement('canvas');
-canvas.id = 'cursorCanvas';
-document.body.appendChild(canvas);
-const ctx = canvas.getContext('2d');
-
-let particles = [];
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
-
-// Создаем частицы дыма с направлением противоположным движению курсора
-function createParticle(x, y, dx, dy) {
-    const angle = Math.atan2(dy, dx) + (Math.random() * Math.PI - Math.PI / 2); // Увеличенный угол рассеивания
-    const speed = Math.random() * 1 + 0.5; // Скорость частиц
-    return {
-        x: x,
-        y: y,
-        size: Math.random() * 20 + 15, // Уменьшенный начальный размер частиц для более компактного эффекта возле курсора
-        speedX: -speed * Math.cos(angle), // Движение противоположное направлению курсора с учетом случайного угла
-        speedY: -speed * Math.sin(angle),
-        opacity: 0.2 + Math.random() * 0.3, // Более высокая начальная прозрачность
-        life: 200 + Math.random() * 200 // Длительное время жизни частицы
-    };
-}
-
-// Рендер частиц с градиентом
-function renderParticles() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    particles.forEach((particle, index) => {
-        particle.x += particle.speedX;
-        particle.y += particle.speedY;
-        particle.size += 0.3; // Медленное увеличение размера частицы
-        particle.opacity -= 0.004; // Медленное затухание прозрачности
-
-        if (particle.opacity <= 0) {
-            particles.splice(index, 1);
-        } else {
-            ctx.beginPath();
-            const gradient = ctx.createRadialGradient(
-                particle.x,
-                particle.y,
-                0,
-                particle.x,
-                particle.y,
-                particle.size
-            );
-            gradient.addColorStop(0, `rgba(148, 0, 211, ${particle.opacity})`); // Сиреневый центр
-            gradient.addColorStop(1, `rgba(255, 20, 147, 0)`); // Прозрачный розовый край
-            ctx.fillStyle = gradient;
-            ctx.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2);
-            ctx.fill();
-        }
-    });
-}
-
-// Добавление новых частиц при движении курсора
-function updateParticles(e) {
-    const dx = e.movementX; // Изменение положения курсора по X
-    const dy = e.movementY; // Изменение положения курсора по Y
-
-    for (let i = 0; i < 15; i++) {
-        particles.push(createParticle(e.clientX, e.clientY, dx, dy));
-    }
-    renderParticles();
-}
-
-// Обработка движения курсора
-window.addEventListener('mousemove', updateParticles);
-window.addEventListener('touchmove', (e) => {
-    if (e.touches.length > 0) {
-        updateParticles(e.touches[0]);
-    }
-});
-
-// Анимация частиц
-function animate() {
-    renderParticles();
-    requestAnimationFrame(animate);
-}
-
-animate();
-
-
+// Проверка на мобильное устройство
+window.mobileAndTabletCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
 
 // прозрачность хедера при прокрутке
-
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
     if (window.scrollY > 50) {
@@ -130,3 +34,11 @@ window.addEventListener('scroll', function () {
     }
 });
 
+// Дым под курсором
+if (window.mobileAndTabletCheck() === false) {
+    const animateSmoke = async function() {
+        const smokeLib = await import('./smoke.js');
+        smokeLib.default();
+    }
+    animateSmoke();
+}
